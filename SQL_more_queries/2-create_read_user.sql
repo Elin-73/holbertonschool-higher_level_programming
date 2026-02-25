943a015e2bd119ae504d338f11f1c9c0dfb9cdb6
Elin-73 <santiagomandujano.aguilar@gmail.com>
2026-02-25 16:56:11 +0000

    HOLBERTON TASK

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- not only does it create a database, but it also creates read users
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- table force name in the new database
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
