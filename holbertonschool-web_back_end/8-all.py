3bf0ce5dc7ba7b8721d12667b4a656caf8e5a5d2
Elin-73 <santiagomandujano.aguilar@gmail.com>
2026-04-14 19:53:34 -0600

    Update 8-all.py

--- holbertonschool-web_back_end/8-all.py
+++ holbertonschool-web_back_end/8-all.py
@@ -2,9 +2,4 @@
 """ 8-all code """
 
 def list_all(mongo_collection):
-    documents: list = []
-
-    for document in mongo_collection.find():
-        documents.append(document)
-
-    return documents
+    return list(mongo_collection.find())
